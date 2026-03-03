4a94b4693d39ed7208b27bc1ce898ac3f2bff948
juliandavidpalacios <julian.david.palacios@estudiantat.upc.edu>
2026-03-03 17:25:35 +0100

    Initial commit

---
+++ calculos.py
@@ -0,0 +1,17 @@
+
+def suma(a, b):
+    return a + b
+def resta(a, b):
+    return a - b
+def multiplicacion(a, b):
+    return a * b
+def division(a, b):
+    return a / b
+
+
+if __name__ == '__main__':
+    print('suma;',suma(4, 5))
+    print('resta:',resta(4, 5))
+    print('multiplcacion:', multiplicacion(4, 5))
+    print('division', division(4, 5)
+          )
