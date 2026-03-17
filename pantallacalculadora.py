10de102e512861147b6e61450b27a900db989ed1
santiago.villar <santiago.villar@estudiantat.upc.edu>
2026-03-17 16:17:30 +0100

    arreglo calculadora

--- pantallacalculadora.py
+++ pantallacalculadora.py
@@ -1,17 +1,25 @@
 from PySide6 import QtWidgets
-from pantalla import Ui_Dialog as form_class
 
-class PantallaCalculadora(QtWidgets.QMainWindow, form_class):
+# Se importa la clase generada por el comando pyside6-uic
+from ui_calculadora import Ui_Calculadora as form_class
+
+
+class Pantalla(QtWidgets.QMainWindow, form_class):
+    """
+    Clase básica que carga la interfaz generada por QtDesigner.
+    Utiliza herencia múltiple para extender QMainWindow [7], [8].
+    """
+
     def __init__(self, parent=None):
-        QtWidgets.QMainWindow.__init__(self, parent)
+        super().__init__(parent)
         self.setupUi(self)
 
-    def opera(self):
-        pass
-    def verificar(self):
-        pass
+
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    window = PantallaCalculadora()
-    window.show()
-    app.exec()
+    import sys
+    from PySide6 import QtWidgets
+
+    app = QtWidgets.QApplication(sys.argv)
+    pantalla = Pantalla()
+    pantalla.show()
+    sys.exit(app.exec())
